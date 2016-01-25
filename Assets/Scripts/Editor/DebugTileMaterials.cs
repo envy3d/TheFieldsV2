bacbbdc1799b487f13355241cfa3cbc43685749a
Nick Cooper <envy3d@gmail.com>
2016-01-25 22:06:40 +1300

    Selecting tile properties now automatically updates the tile's debug color

---
+++ Assets/Scripts/Editor/DebugTileMaterials.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+[CreateAssetMenu]
+public class DebugTileMaterials : ScriptableObject
+{
+    public Material landmineMat;
+    public Material startMat;
+    public Material finishMat;
+    public Material traversableMat;
+    public Material defaultMat;
+}
---
+++ Assets/Scripts/Editor/TileScriptEditor.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TileScript))]
+[CanEditMultipleObjects]
+public class TileScriptEditor : Editor
+{
+    TileProperty oldTileProperties;
+    SerializedProperty tileProperties;
+    DebugTileMaterials tileMaterials;
+
+    void OnEnable()
+    {
+        var tileMaterialsGUID = AssetDatabase.FindAssets("EditorTileMaterials")[0];
+        var tileMaterialsPath = AssetDatabase.GUIDToAssetPath(tileMaterialsGUID);
+        tileMaterials = AssetDatabase.LoadAssetAtPath<DebugTileMaterials>(tileMaterialsPath);
+        tileProperties = serializedObject.FindProperty("properties");
+        oldTileProperties = (TileProperty)tileProperties.intValue;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        serializedObject.Update();
+        TileProperty tp = (TileProperty)tileProperties.intValue;
+        
+        if (oldTileProperties != tp)
+        {
+            var renderer = ((TileScript)serializedObject.targetObject).GetComponent<Renderer>();
+            if ((tp & TileProperty.Landmine) == TileProperty.Landmine)
+            {
+                renderer.sharedMaterial = tileMaterials.landmineMat;
+            }
+            else if ((tp & TileProperty.Start) == TileProperty.Start)
+            {
+                renderer.sharedMaterial = tileMaterials.startMat;
+            }
+            else if ((tp & TileProperty.Finish) == TileProperty.Finish)
+            {
+                renderer.sharedMaterial = tileMaterials.finishMat;
+            }
+            else if ((tp & TileProperty.Traversable) == TileProperty.Traversable)
+            {
+                renderer.sharedMaterial = tileMaterials.traversableMat;
+            }
+            else
+            {
+                renderer.sharedMaterial = tileMaterials.defaultMat;
+            }
+        }
+        oldTileProperties = tp;
+    }
+}
